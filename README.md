b7ff8c0b812b3ca949ad795881d1c08b4e69c2a9
HeshanKarunaratne <heshan947102@gmail.com>
2023-07-05 15:37:52 +0530

    Scene Builder init and example1.fxml

--- README.md
+++ README.md
@@ -34,3 +34,47 @@ public class Main extends Application {
     }
 }
 ~~~
+
+- Added various components
+~~~java
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 600);
+
+        Text text = new Text();
+        text.setText("Hi!!");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Verdana", 50));
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(200);
+        rectangle.setWidth(200);
+        rectangle.setX(400);
+        rectangle.setY(400);
+
+        root.getChildren().add(text);
+        root.getChildren().add(rectangle);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+~~~
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,39 +1,18 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 600,600);
-
-        Text text = new Text();
-        text.setText("Hi!!");
-        text.setX(50);
-        text.setY(50);
-        text.setFont(Font.font("Verdana", 50));
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.setHeight(200);
-        rectangle.setWidth(200);
-        rectangle.setX(400);
-        rectangle.setY(400);
-
-        root.getChildren().add(text);
-        root.getChildren().add(rectangle);
-
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("example1.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 

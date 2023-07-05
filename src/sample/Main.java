af755a23fce930f68f1c071f571de58397d05b61
HeshanKarunaratne <heshan947102@gmail.com>
2023-07-05 16:04:32 +0530

    example2: Handling click events

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("example1.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("example2.fxml"));
         stage.setTitle("Hello World");
         stage.setScene(new Scene(root));
         stage.show();

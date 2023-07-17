95eb810237530bc081dda87ebdaf0d1336efbc2f
HeshanKarunaratne <heshan947102@gmail.com>
2023-07-17 17:40:03 +0530

    Auto Suggestion ListView Example

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,57 +1,95 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+//    @Override
+//    public void start(Stage stage) {
+//        try {
+//
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource("example13.fxml"));
+//            Parent root = loader.load();
+//
+//            KeyEventsController controller = loader.getController();
+//            Scene scene = new Scene(root);
+//
+//            scene.setOnKeyPressed(keyEvent -> {
+//                switch (keyEvent.getCode()){
+//                    case UP:
+//                        controller.moveUp();
+//                        break;
+//                    case DOWN:
+//                        controller.moveDown();
+//                        break;
+//                    case LEFT:
+//                        controller.moveLeft();
+//                        break;
+//                    case RIGHT:
+//                        controller.moveRight();
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            });
+//
+//            stage.setScene(scene);
+//            stage.show();
+//
+//            stage.setOnCloseRequest(event -> {
+//                logout(stage);
+//                event.consume();
+//            });
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    private ObservableList<String> suggestions = FXCollections.observableArrayList(
+            "Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew", "Jackfruit"
+    );
+
     @Override
-    public void start(Stage stage) {
-        try {
+    public void start(Stage primaryStage) {
+        TextField textField = new TextField();
+        ListView<String> listView = new ListView<>();
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("example13.fxml"));
-            Parent root = loader.load();
+        // Set up the list view with the suggestions
+        listView.setItems(suggestions);
 
-            KeyEventsController controller = loader.getController();
-            Scene scene = new Scene(root);
+        // Event handler to filter the suggestions based on the user's input
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            String filter = newValue.toLowerCase();
+            ObservableList<String> filteredList = FXCollections.observableArrayList();
 
-            scene.setOnKeyPressed(keyEvent -> {
-                switch (keyEvent.getCode()){
-                    case UP:
-                        controller.moveUp();
-                        break;
-                    case DOWN:
-                        controller.moveDown();
-                        break;
-                    case LEFT:
-                        controller.moveLeft();
-                        break;
-                    case RIGHT:
-                        controller.moveRight();
-                        break;
-                    default:
-                        break;
+            for (String suggestion : suggestions) {
+                if (suggestion.toLowerCase().contains(filter)) {
+                    filteredList.add(suggestion);
                 }
-            });
+            }
 
-            stage.setScene(scene);
-            stage.show();
+            listView.setItems(filteredList);
+        });
 
-            stage.setOnCloseRequest(event -> {
-                logout(stage);
-                event.consume();
-            });
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        VBox root = new VBox(textField, listView);
+        Scene scene = new Scene(root, 200, 300);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Auto Suggestion ListView Example");
+        primaryStage.show();
     }
 
     public void logout(Stage stage) {
